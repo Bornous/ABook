e3fe3849a97acabf36629a03e353ed69dfcee08f
Bornous <pavelgra77@gmail.com>
2018-12-20 14:08:02 +0100

    Color changong

--- firstPage.php
+++ firstPage.php
@@ -13,28 +13,31 @@
 		  });
 		});
 	</script>
+	<div class="book">
+	<?php
 
-<?php
-
-	echo	"<div class='revealedText' ><span class='justText'>Przed wiekami...</span></div>";
-	echo	"<div class='revealedText' ><span class='justText'>Znaczy tymi, które nadejdą...</span></div>";
-	echo	"<div class='revealedText' ><span class='justText'>Czyli no w sumie teraz, niosąc świeżo wykuty miecz od kowala, Darias nerwowo rozglądał się na wszystkie strony licząc, że nie napotka osoby, o której ciągle myślał. Los jednak bywa złośliwy i za rogiem dobrze znanej wszystkim karczmie wpadł na rzeczonego jegomościa. Oczy rozszerzyły się gwałtownie w geście zaskoczenia, usta wymamrotały szybkie \"Przepraszasm najmocniej\", jednak drugie spojrzenie przyniosło rozpacz w sercu Dariasa. Spostrzegł on iż potrącony przechodzień to w istocie jego brat.</span></div>";
-	
-	echo	"<div class='redText' ><span class='dialog_2'>Pozdrowiony! Powinieneś bardziej zwracać uwagę na otoczenie, jeśli chcesz się zabrać za szermierkę. Ale mniejsza, 
-	słyszałeś?</span></div>";
-	echo	"<div class='redText' ><span class='dialog_1'>O czym?</span></div>";
-	echo	"<div class='redText' ><span class='dialog_2'>Co się stało z Carilm zaraz po świętach.</span></div>";
-	echo	"<div class='redText' ><span class='dialog_1'>Nie. I szczerze mówiąc niewiele mnie to obchodzi.</span></div>";
-			
-			
-	
-
-
+		echo	"<div class='revealedText' ><span class='justText'>Przed wiekami...</span></div>";
+		echo	"<div class='revealedText' ><span class='justText'>Znaczy tymi, które nadejdą...</span></div>";
+		echo	"<div class='revealedText' ><span class='justText'>Czyli no w sumie teraz, niosąc świeżo wykuty miecz od kowala, Darias nerwowo rozglądał się na wszystkie strony. Liczył, że nie napotka osoby, o której ciągle myślał. Los jednak bywa złośliwy i za rogiem dobrze znanej wszystkim karczmie wpadł na rzeczonego jegomościa. Oczy rozszerzyły się gwałtownie w geście zaskoczenia, usta wymamrotały szybkie \"Przepraszasm najmocniej\", jednak drugie spojrzenie przyniosło rozpacz w sercu Dariasa. Spostrzegł on iż potrącony przechodzień to w istocie jego brat.</span></div>";
+		
+		echo	"<div class='redText' ><span class='dialog_2'>Pozdrowiony! Powinieneś bardziej zwracać uwagę na otoczenie, jeśli chcesz się zabrać za szermierkę. Ale mniejsza, 
+		słyszałeś?</span></div>";
+		echo	"<div class='redText' ><span class='dialog_1'>O czym?</span></div>";
+		echo	"<div class='redText' ><span class='dialog_2'>Co się stało z Carilim zaraz po świętach.</span></div>";
+		echo	"<div class='redText' ><span class='dialog_1'>Nie. I szczerze mówiąc niewiele mnie to obchodzi.</span></div>";
+				
+				
+		
 
 
 
 
 
-?>
+
+
+	?>
+	</div>
+	<div class="right_runes"><img src="graphics/long_stand_bar_with_places.png"/></div>
+	<div class="clear_both"></div>
 </body>
 </html>
